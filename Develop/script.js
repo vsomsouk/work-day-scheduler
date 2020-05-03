504104631e4e4f2733d9b1445aa945b6f283a9de
Vanida Somchaleunsouk <vsomsouk@gmail.com>
2020-05-02 19:32:11 -0500

    Added variables for JS file

---
+++ Develop/script.js
@@ -0,0 +1,19 @@
+// Create variables.
+
+var nineAmTime = document.querySelector("#nine-am");
+var tenAmTime = document.querySelector("#ten-am");
+var elevenAmTime = document.querySelector("#eleven-am");
+var twelveAmTime = document.querySelector("#twelve-am");
+var onePmTime = document.querySelector("#one-pm");
+var twoPmTime = document.querySelector("#two-pm");
+var threePmTime = document.querySelector("#three-pm");
+var fourPmTime = document.querySelector("#four-pm");
+var fivePmTime = document.querySelector("#five-pm")
+
+var saveBtn = document.querySelector("#save");
+
+// Save characters typed in text area in local storage.
+// Assign color code for each time slot when the hour has passed.
+// Assign color code in text area for the current time in time slot.
+// Assign color code for each time slot when time hasn't passed (future).
+// Create live display date and time under header.
